0aa35209ff6ccb603b1d2b46f4b1761227ec81d4
mjung8 <mmkjung@gmail.com>
2019-05-14 22:02:34 -0400

    Add Episode 5 content change tile with click and click and drag

--- Assets/Controllers/MouseController.cs
+++ Assets/Controllers/MouseController.cs
@@ -1,26 +1,79 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class MouseController : MonoBehaviour {
+public class MouseController : MonoBehaviour
+{
 
     public GameObject circleCursor;
 
     Vector3 lastFramePosition;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    Vector3 dragStartPosition;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
         Vector3 currentFramePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         currentFramePosition.z = 0;
 
         // Update the circle cursor position
-        circleCursor.transform.position = currentFramePosition;
+        Tile tileUnderMouse = GetTileAtWorldCoord(currentFramePosition);
+        if (tileUnderMouse != null)
+        {
+            circleCursor.SetActive(true);
+            Vector3 cursorPosition = new Vector3(tileUnderMouse.X, tileUnderMouse.Y, 0);
+            circleCursor.transform.position = cursorPosition;
+        }
+        else
+        {
+            circleCursor.SetActive(false);
+        }
+
+        // Start Drag
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragStartPosition = currentFramePosition;
+        }
+
+        // End Drag
+        if (Input.GetMouseButtonUp(0))
+        {
+            int start_x = Mathf.FloorToInt(dragStartPosition.x);
+            int end_x = Mathf.FloorToInt(currentFramePosition.x);
+            if (end_x < start_x)
+            {
+                int tmp = end_x;
+                end_x = start_x;
+                start_x = tmp;
+            }
+
+            int start_y = Mathf.FloorToInt(dragStartPosition.y);
+            int end_y = Mathf.FloorToInt(currentFramePosition.y);
+            if (end_y < start_y)
+            {
+                int tmp = end_y;
+                end_y = start_y;
+                start_y = tmp;
+            }
+
+            for (int x = start_x; x <= end_x; x++)
+            {
+                for (int y = start_y; y <= end_y; y++)
+                {
+                    Tile t = WorldController.Instance.World.GetTileAt(x, y);
+                    if (t != null)
+                    {
+                        t.Type = Tile.TileType.Floor;
+                    }
+                }
+            }
+        }
 
         // Handle screen dragging
         if (Input.GetMouseButton(1) || Input.GetMouseButton(2)) // Right or middle mouse button
@@ -31,5 +84,13 @@ public class MouseController : MonoBehaviour {
 
         lastFramePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         lastFramePosition.z = 0;
-	}
+    }
+
+    Tile GetTileAtWorldCoord(Vector3 coord)
+    {
+        int x = Mathf.FloorToInt(coord.x);
+        int y = Mathf.FloorToInt(coord.y);
+
+        return WorldController.Instance.World.GetTileAt(x, y);
+    }
 }
--- Assets/Controllers/WorldController.cs
+++ Assets/Controllers/WorldController.cs
@@ -4,24 +4,33 @@ using System;
 
 public class WorldController : MonoBehaviour {
 
+    public static WorldController Instance { get; protected set; }
+
     // Simple way to handle current sprite
     public Sprite floorSprite;
 
     // World and tile data
-	World world;
+	public World World { get; protected set; }
 
 	// Use this for initialization
 	void Start () {
+
+        if (Instance != null)
+        {
+            Debug.LogError("There should never be two world controllers.");
+        }
+        Instance = this;
+
         // Create a world with Empty tiles
-		world = new World();
+		World = new World();
 
         // Create a GameObject for each of our tiles, so they show visually
-        for (int x = 0; x < world.Width; x++)
+        for (int x = 0; x < World.Width; x++)
         {
-            for (int y = 0; y < world.Height; y++)
+            for (int y = 0; y < World.Height; y++)
             {
                 // Get the tile data
-                Tile tile_data = world.GetTileAt(x, y);
+                Tile tile_data = World.GetTileAt(x, y);
                 // Create a new GameObject and add to the scene
                 GameObject tile_go = new GameObject();
                 tile_go.name = "Tile_ " + x + "_" + y;
@@ -38,7 +47,7 @@ public class WorldController : MonoBehaviour {
         }
 
         // For testing
-        world.RandomizeTiles(); 
+        World.RandomizeTiles(); 
 
 	}
 
